ca5e2ed3395bd608b7ce2cf6045aa98bc4086e2e
ddebutts <dan.debutts@gmail.com>
2019-10-30 20:33:36 -0400

    fixed code to set backgound input colors and local storage retrieve and set

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Day-Planner
 Day-Planner that allows you to save hourly events from 9am to 5pm
+Updated for past present and future classes to change color for enent inputs
+Updated the local storage for retrieving and setting 
+
+This daily planner is now a useful tool.
--- javascript.js
+++ javascript.js
@@ -1,6 +1,29 @@
 var timeSlots = [];
 var d = new Date();
 
+// var timeObjSTRING = localStorage.getItem("timeObj")
+// var timeObj = JSON.parse(timeObjSTRING)
+// console.log(timeObj, timeObjSTRING)
+
+// for(var i in timeObj){
+//     console.log(i, timeObj[i])
+//     var time = $("[data-time=" + i + "]");
+//     var myMoment = moment(i, "H")
+//     var eventIntput = time.parent().next().find('.form-control');
+
+//     eventIntput.val(timeObj[i])
+    
+//     if(moment().isAfter(myMoment)){
+//         console.log("past")
+//         eventIntput.addClass("past")
+//     } else if(moment().isSame(myMoment, 'hour')){
+//         console.log("present")
+//         eventIntput.addClass("present")
+//     } else if(moment().isBefore(myMoment)){
+//         console.log("future")
+//         eventIntput.addClass("future")
+//     }
+// }
 
 //populate todays date time in the header
 document.getElementById("tdate").innerHTML = d.toDateString();
@@ -11,54 +34,69 @@ Start()
 
 
 
-function Start(){
+function Start() {
     timeSlots = getTimeArr();
     console.log(timeSlots);
     populateTimeSlots(timeSlots);
-    
 }
 
-function getTimeArr(){
+function getTimeArr() {
     let tempArr = [];
-    for(var i = 0; i < 9; i++){
+    for (var i = 0; i < 9; i++) {
         var hour = convNumToHour(i);
         var time = {
-            time: hour.format("h:mm-a"),
-            event: localStorage.getItem(event)
+            time: hour,
+            event: localStorage.getItem(hour.format("H"))
         };
         tempArr.push(time);
-        
+
     };
     return tempArr;
 };
 
-function convNumToHour(number){
+function convNumToHour(number) {
     return moment(number + 9, "HH");
 };
 
-function populateTimeSlots(timeSlots){
+function populateTimeSlots(timeSlots) {
     timeSlots.forEach(timeslot => {
-      
-     console.log(timeslot.time);
-     console.log(timeslot.event);
+
+        var hour = timeslot.time.format("H");
+        var time = $("[data-time=" + hour + "]");
+        var eventIntput = time.parent().next().find('.form-control');
+
+        eventIntput.val(timeslot.event)
+        
+        if(moment().isAfter(timeslot.time)){
+            console.log("past")
+            eventIntput.addClass("past")
+        } else if(moment().isSame(timeslot.time, 'hour')){
+            console.log("present")
+            eventIntput.addClass("present")
+        } else if(moment().isBefore(timeslot.time)){
+            console.log("future")
+            eventIntput.addClass("future")
+        }
+
+        console.log(timeslot.event);
     });
 };
 
+var data = {
+    name: "ben"
+}
 
+console.log(data.name)
+console.log(data["name"])
 
-// timeSlots.forEach((value) => {
-//    console.log("Time: " + value.time + "Event: " + value.event);
-   
-// });
+$(".btn").on("click", function () {
+    var hour = $(this).parent().parent().find('.time').children().attr("data-time")
+    var event = $(this).parent().parent().find('.event').children().val()
 
-function PastPresentFuture(){
-    //compare current time to slot time and update color accordingly
-};
+    timeObj[hour] = event;
+    localStorage.setItem("timeObj", JSON.stringify(timeObj))
 
-$("saveButton").on("click", function() {
-    event.preventDefault();
-    
-    var event = $(this).attr("data-event");
-
-
+    console.log(hour, event)
+    localStorage.setItem(hour, event)
+    //var input = document.getElementById('userInput').value; 
 });
